c6e396df428df9f90e0ec69254b69a9425e1edb8
Harri Lammi <harri.lammi@leonidasoy.fi>
2011-09-17 01:05:17 +0300

    Almost got it.

--- basket.rb
+++ basket.rb
@@ -2,35 +2,40 @@ class Basket
 
   def initialize
     @titles = {"one" => 0, "two" => 0, "three" => 0, "four" => 0, "five" => 0}
+    @discounts = [1, 0.95, 0.90, 0.80, 0.75]
   end
 
   def add(titles)
   	titles.each { |title| @titles[title] += 1 }
   end
 
-  def total
-  	total = 0
-  	if number_of_books == 2 && number_of_different_books == 2
-  		total = 15.2
+  def price
+  	price = 0
+  	books, different_books = book_stats
+
+  	if books == different_books
+  		price = books * 8 * @discounts[different_books-1]
+  	elsif different_books == 1
+  		price = books * 8 
+  	elsif books % different_books == 0
+  		factor = books / different_books
+  		price = factor * different_books * 8 * @discounts[different_books-1]
   	else
-  		total = 8 * number_of_books
+  		price = different_books * 8 * @discounts[different_books-1]
+  		price += (books - different_books) * 8 		
   	end
-  	total
+  	price
   end
 
-  def number_of_books
+  def book_stats
   	books = 0
-  	@titles.keys.each {|title| books += @titles[title] }
-  	books
-  end
-
-  def number_of_different_books
-	books = 0
-  	@titles.keys.each do |title|
-  		if @titles[title] > 0
-  			books += 1
+  	different_books = 0
+  	@titles.keys.each do |title|  	 	
+  	 	if @titles[title] > 0
+  	 		books += @titles[title]
+  			different_books += 1
   		end
   	end
-  	books
+  	[books, different_books]
   end
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -2,29 +2,63 @@ require './basket'
 
 describe Basket do 
 
-  describe "basket" do  
-  	it "should store added titles" do
-	end
+  before :each do
+    @basket = Basket.new
   end
-  
-  describe "discount" do
-  	before :each do
-  	  @basket = Basket.new
-  	end
 
-  	it "should be zero for one book" do
-  		@basket.add(["four"])
-  		@basket.total.should == 8
-  	end
-
-    it "should not give discount for two same books" do
-      @basket.add(["one", "one"])
-      @basket.total.should == 16
+  describe "with no discount" do
+    it "should cost 8e for one book" do
+      @basket.add(["four"])
+      @basket.price.should == 8
     end
 
+    it "should cost 16e for two books of the same title" do
+      @basket.add(["four", "four"])
+      @basket.price.should == 2*8
+    end
+
+    it "should cost 24e for three books of the same title" do
+      @basket.add(["four", "four", "four"])
+      @basket.price.should == 3*8
+    end
+  end
+  
+  describe "with simple discounts" do
   	it "should be 5% for 2 different titles" do
   		@basket.add(["one","three"])
-  		@basket.total.should == 15.2
+  		@basket.price.should == 8*2*0.95
   	end
+
+    it "should be 10% for 3 different titles" do
+      @basket.add(["one","three", "five"])
+      @basket.price.should == 3*8*0.9
+    end
+
+    it "should be 20% for 4 different titles" do
+      @basket.add(["one","three", "five", "two"])
+      @basket.price.should == 4*8*0.8
+    end
+
+    it "should be 25% for 5 different titles" do
+      @basket.add(["one","three", "five", "two", "four"])
+      @basket.price.should == 5*8*0.75
+    end
+  end
+
+  describe "several discounts" do
+    it "should be 5% for 3 books from which 2 are same titles" do
+      @basket.add(["one", "five", "one"])
+      @basket.price.should == 8+8*2*0.95
+    end
+
+    it "should be 5% for 4 books which are 2 different titles" do
+      @basket.add(["one", "five", "five", "one"])
+      @basket.price.should == 2*8*2*0.95
+    end
+
+    it "should calculate best discount with several titles" do
+      @basket.add(["one", "one", "two", "three", "three", "four"])
+      @basket.price.should == 8*4*0.8+8*2*0.95
+    end
   end
 end
